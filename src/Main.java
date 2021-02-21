3726a559a37ae855d936f80471d6bfdb78f359f7
Eddie <eddiefu8@gmail.com>
2021-02-20 16:01:50 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -34,9 +34,13 @@ public class Main {
             LocalDate date = LocalDate.now(); // Today's date
             date = date.minusDays(1);
             for (int i = 0; i < aList.size(); i++) { // search through all list items
+                // check for overdue/incomplete assignments
                 if (date.equals(aList.get(i).getDate())) { // check if date objects are equaivalent
                     francis.healthDown(missedAssignmentPenalty); // lower Tamagotchi's health
+                    aList.remove(i);
                 }
+                // check for assignments completed early
+                if (aList.get(i).)
             }
         }
     }
